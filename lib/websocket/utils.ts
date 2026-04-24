589ca858bd472f26dfdb8239aa21fecf9d4ae0bf
emmanuel iheanacho <emmanueliheanacho04@gmail.com>
2026-04-24 09:17:52 +0100

    cl fix

--- lib/websocket/utils.ts
+++ lib/websocket/utils.ts
@@ -116,7 +116,7 @@ export function notifyWebSocketWalletEvent(action: "connect" | "disconnect", wal
  */
 export function getWebSocketStatus() {
   const client = getWebSocketClient()
-  return client.getConnectionState()
+  return client.isConnected()
 }
 
 /**
